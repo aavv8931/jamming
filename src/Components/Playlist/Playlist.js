7b49f21b033243a2dafdd0046f2ffa8f0233e7a3
aavv8931 <arturo_veintimilla@hotmail.com>
2022-06-30 08:02:20 -0500

    add Playlist component props

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './Playlist.css';
-// import TrackList from "../TrackList/TrackList";
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component{
     render(){
         return (
             <div class="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                {/* <TrackList /> */}
+                <TrackList tracks={this.props.playlistTracks}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
