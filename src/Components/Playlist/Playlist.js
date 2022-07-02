a327ada480f4f2be08b727d35d715edc0fc99865
aavv8931 <arturo_veintimilla@hotmail.com>
2022-07-02 10:51:18 -0500

    add onClick attribute to button in Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends React.Component{
         super(props)
         this.handleNameChange = this.handleNameChange.bind(this)
     }
-    
+
     handleNameChange(e){
         this.props.onNameChange(e.target.value)
     }
@@ -17,7 +17,7 @@ class Playlist extends React.Component{
             <div class="Playlist">
                 <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
     }
