1880eb82d3c504194b4b9748a78ea16565453cde
aavv8931 <arturo_veintimilla@hotmail.com>
2022-07-02 12:16:10 -0500

    add constructor & methods

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,26 @@ import React from "react";
 import './SearchBar.css'
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props)
+        this.search = this.search.bind(this)
+        this.handleTermChange = this.handleTermChange.bind(this)
+
+        this.set = {string: ''}
+    }
+    search(){
+        this.props.onSearch(this.state.string)
+    }
+
+    handleTermChange(e){
+        this.setState({string: e.target.value})
+    }
+
     render(){
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+                <button className="SearchButton" onSearch={this.props.onSearch}>SEARCH</button>
             </div>
         )
     }
